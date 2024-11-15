a626d70ddba59f7582d9f890a82f6c4d56331185
gakjee <oliverparsonst@gmail.com>
2024-11-15 09:54:14 +0000

    Added SimpleFormatter import

--- src/test/java/com/example/Dec2HexTest.java
+++ src/test/java/com/example/Dec2HexTest.java
@@ -10,6 +10,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Dec2HexTest {
 
