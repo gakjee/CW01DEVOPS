b68285e9cbfc689105759bf8c5d24679d3db9c5c
gakjee <oliverparsonst@gmail.com>
2024-11-15 12:42:21 +0000

    Update 8

--- src/main/java/com/example/Dec2Hex.java
+++ src/main/java/com/example/Dec2Hex.java
@@ -4,20 +4,28 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import java.util.logging.Handler;
 
 public class Dec2Hex {
 
     private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
-    public static void main(String[] args) {
-        // Set up logger
+    static {
+        // Set up logger only once using static initializer
         logger.setUseParentHandlers(false);
         ConsoleHandler handler = new ConsoleHandler();
         handler.setFormatter(new SimpleFormatter());
         handler.setLevel(Level.ALL);
         logger.addHandler(handler);
         logger.setLevel(Level.ALL);
+    }
 
+    // Method to add a custom handler
+    public static void addHandler(Handler handler) {
+        logger.addHandler(handler);
+    }
+
+    public static void main(String[] args) {
         if (args.length == 0) {
             logger.severe("Please provide a decimal number as an argument.");
             return;
